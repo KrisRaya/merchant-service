6dcecfa9f13ea25f400af0e2bca9e8e082d98fb6
KrisRaya <krisraya13@gmail.com>
2020-09-03 16:50:56 +0800

    implement swagger

--- src/main/java/com/demo/merchantservice/MerchantServiceApplication.java
+++ src/main/java/com/demo/merchantservice/MerchantServiceApplication.java
@@ -3,13 +3,31 @@ package com.demo.merchantservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class MerchantServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MerchantServiceApplication.class, args);
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
