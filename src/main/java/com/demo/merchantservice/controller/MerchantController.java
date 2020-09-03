27dcdb839b7198536a640d973ffc264ddd212523
KrisRaya <krisraya13@gmail.com>
2020-09-03 16:48:51 +0800

    Add merchant controller

--- src/main/java/com/demo/merchantservice/controller/MerchantController.java
+++ src/main/java/com/demo/merchantservice/controller/MerchantController.java
@@ -1,13 +1,56 @@
 package com.demo.merchantservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.demo.merchantservice.common.ErrorMessage;
+import com.demo.merchantservice.common.ResponseWrapper;
+import com.demo.merchantservice.model.Merchant;
+import com.demo.merchantservice.service.MerchantService;
+import com.demo.merchantservice.validator.MerchantValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 public class MerchantController {
 
-    @GetMapping("/ping")
-    public String ping() {
-        return "pong";
+    private static final String STATUS = "status";
+    @Autowired
+    private MerchantService service;
+
+    @Autowired
+    private MerchantValidator validator;
+
+    @GetMapping("/findById/{id}")
+    public Merchant findMerchantById(@PathVariable Long id) {
+        return service.findById(id).orElse(null);
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<ResponseWrapper> findAllMerchant() {
+        final List<Merchant> merchants = service.findAll();
+        return ResponseEntity.ok(new ResponseWrapper(merchants, Collections.singletonMap(STATUS, HttpStatus.OK)));
+    }
+
+    @PostMapping("/addMerchant")
+    public ResponseEntity<ResponseWrapper> addMerchant(@RequestBody Merchant merchant) {
+        final List<ErrorMessage> errorMessages = validator.validateMerchant(merchant);
+        if (!errorMessages.isEmpty()) {
+            return new ResponseEntity(new ResponseWrapper(Collections.singletonMap(STATUS, HttpStatus.NOT_ACCEPTABLE), errorMessages), HttpStatus.NOT_ACCEPTABLE);
+        }
+        final Merchant addedMerchant = service.addMerchant(merchant);
+        return ResponseEntity.ok(new ResponseWrapper(addedMerchant, Collections.singletonMap(STATUS, HttpStatus.OK)));
+    }
+
+    @PostMapping("/payMerchant")
+    public ResponseEntity<ResponseWrapper> payMerchant(@RequestBody Merchant merchant) {
+        final List<ErrorMessage> errorMessages = validator.validateMerchantIdExist(merchant);
+        if (!errorMessages.isEmpty()) {
+            return new ResponseEntity(new ResponseWrapper(Collections.singletonMap(STATUS, HttpStatus.NOT_ACCEPTABLE), errorMessages), HttpStatus.NOT_ACCEPTABLE);
+        }
+        final Merchant payMerchant = service.payMerchant(merchant);
+        return ResponseEntity.ok(new ResponseWrapper(payMerchant, Collections.singletonMap(STATUS, HttpStatus.OK)));
     }
 }
